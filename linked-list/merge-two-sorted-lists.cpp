f06209eb4566d323da62e7f829a1934ea90f1652
aayushbhandaru12 <aayushbhandari462@gmail.com>
2026-09-09 22:10:46 +0530

    LC-merge-two-sorted-lists [Runtime: N/A; Memory: N/A]

--- linked-list/merge-two-sorted-lists.cpp
+++ linked-list/merge-two-sorted-lists.cpp
@@ -1,26 +1,22 @@
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);
-        ListNode* current = &dummy;
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode(-1)
+        current = dummy
 
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                current->next = list1;
-                list1 = list1->next;
-            } else {
-                current->next = list2;
-                list2 = list2->next;
-            }
+        while list1 and list2:
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
 
-            current = current->next;
-        }
+            current = current.next
 
-        if (list1 != nullptr)
-            current->next = list1;
-        else
-            current->next = list2;
+        # Attach the remaining nodes
+        if list1:
+            current.next = list1
+        else:
+            current.next = list2
 
-        return dummy.next;
-    }
-};
+        return dummy.next
