f51ad20cf54c79b277be3ae5e373b6043b72fb02
aayushbhandaru12 <aayushbhandari462@gmail.com>
2026-09-12 22:24:50 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))
+        if (x < 0)
             return false;
 
-        int rev = 0;
+        int original = x;
+        long long reverse = 0;
 
-        while (x > rev) {
-            rev = rev * 10 + x % 10;
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
             x /= 10;
         }
 
-        return (x == rev || x == rev / 10);
+        return original == reverse;
     }
 };
