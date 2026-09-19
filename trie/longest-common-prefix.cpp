7a29a26610e87ce183310f4197845be808f7f737
aayushbhandaru12 <aayushbhandari462@gmail.com>
2026-09-19 22:28:57 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- trie/longest-common-prefix.cpp
+++ trie/longest-common-prefix.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
-
-        string prefix = strs[0];
+        string ans = strs[0];
 
         for (int i = 1; i < strs.size(); i++) {
-            while (strs[i].find(prefix) != 0) {
-                prefix.pop_back();
-                if (prefix.empty())
-                    return "";
+            int j = 0;
+
+            while (j < ans.size() && 
+                   j < strs[i].size() && 
+                   ans[j] == strs[i][j]) {
+                j++;
             }
+
+            ans = ans.substr(0, j);
         }
 
-        return prefix;
+        return ans;
     }
 };
